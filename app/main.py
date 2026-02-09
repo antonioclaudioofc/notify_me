6f33da065ccf4f87d2555bdb546ab6e5b3864b27
Antonio Claudio <claudioalvesdev@gmail.com>
2026-02-09 11:24:10 -0300

    feat: add home route

--- app/main.py
+++ app/main.py
@@ -2,13 +2,23 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.modules.contact.router import router as contact_router
 
-app = FastAPI(title="Notify Me API", description="API para receber mensagens de contato e enviar notificações por email.")
+app = FastAPI(
+    title="Notify Me API",
+    description="API para receber mensagens de contato e enviar notificações por email."
+)
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+
+@app.get("/")
+def home():
+    return {"message": "Welcome to the Notify Me API"}
+
 
 app.include_router(contact_router)
